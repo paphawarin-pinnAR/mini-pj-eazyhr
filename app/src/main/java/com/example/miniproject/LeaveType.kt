b377592f68f5d84a8fdf22b29332f68ec2e6c1df
Paphawarin(Mind) <paphawarin@micware-ap.com>
2025-07-16 18:36:09 +0700

    [Add] Create an enum class.

---
+++ app/src/main/java/com/example/miniproject/LeaveType.kt
@@ -0,0 +1,9 @@
+package com.example.miniproject
+
+enum class LeaveType (val displayType: String) {
+    ANNUAL("Annual (P)"),
+    PRIVATE_LEAVE("Private Leave (NP)"),
+    SICK("Sick (P)"),
+    SPECIAL_HOLIDAY("Special Holiday(P)"),
+    NONE("No type selection")
+}
---
+++ app/src/main/java/com/example/miniproject/PeriodType.kt
@@ -0,0 +1,8 @@
+package com.example.miniproject
+
+enum class PeriodType(val displayPeriod:String) {
+    AM("AM"),
+    PM("PM"),
+    FULL_DAY("Full Day"),
+    NONE("No period selection")
+}
