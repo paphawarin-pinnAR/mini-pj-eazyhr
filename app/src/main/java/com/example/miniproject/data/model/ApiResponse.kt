2accec0a9599f54588717c40ded427aa0a7959f1
Paphawarin(Mind) <paphawarin@micware-ap.com>
2025-07-29 15:46:21 +0700

    [Edit] Add new data class; AttendanceData into the ApiResponse.kt

--- app/src/main/java/com/example/miniproject/data/model/ApiResponse.kt
+++ app/src/main/java/com/example/miniproject/data/model/ApiResponse.kt
@@ -11,3 +11,20 @@ data class ClockInData(
     val clockInTime: Long,
     val attendanceDate: Long
 )
+
+data class AttendanceData(
+    val id: Int,
+    val attendanceDate: Long,
+    val clockInTime: Long,
+    val clockOutTime: Long?,
+    val breakDurationMinutes: Int,
+    val totalHours: Double?,
+    val overtimeHours: Int,
+    val status: String,
+    val note: String?,
+    val createdAt: String,
+    val updatedAt: String,
+    val formattedWorkingHours: String,
+    val clockedIn: Boolean,
+    val clockedOut: Boolean
+)
