b9a4e8197413607748f272fc8a0f90b57260c433
Paphawarin(Mind) <paphawarin@micware-ap.com>
2025-08-07 11:31:01 +0700

    [Add] Create the SecondActivity.kt file, activity_second.xml file.

--- app/src/main/java/com/example/miniproject/ui/view/activity/SecondActivity.kt
+++ app/src/main/java/com/example/miniproject/ui/view/activity/SecondActivity.kt
@@ -1,6 +1,10 @@
 package com.example.miniproject.ui.view.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +12,31 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.miniproject.R
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var btnPrevious : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
+
+        initViews()
+        setupButtonListener()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun initViews() {
+        btnPrevious = findViewById(R.id.back_button)
+    }
+
+    private fun setupButtonListener(){
+        btnPrevious.setOnClickListener {
+            finish()
         }
     }
 }
