35f71d24a1aaa0a3efb3b345aa309a75a45e511e
Paphawarin(Mind) <paphawarin@micware-ap.com>
2025-08-19 16:13:18 +0700

    [Edit] Modify the processes in the DayAdapter.kt and MonthAdapter.kt files.

--- app/src/main/java/com/example/miniproject/ui/adapter/MonthAdapter.kt
+++ app/src/main/java/com/example/miniproject/ui/adapter/MonthAdapter.kt
@@ -25,10 +25,15 @@ class MonthAdapter (
     private val dayClickListener: OnDayClickListener)
     : RecyclerView.Adapter<MonthAdapter.MonthViewHolder>() {
 
+    private val dayAdapters = mutableMapOf<Int, DayAdapter>()  //To store tha DayAdapter for all months
+
+    private var previousSelectedMonth: Int? = null
+    private var previousSelectedDate: LocalDate? = null
+
+    //Create the ViewHolder for each item_month.xml //use LayoutInflater to convert XML to View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonthViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_month, parent, false)
         return MonthViewHolder(view)
-        // สร้าง ViewHolder สำหรับแต่ละ item_month.xml //ใช้ LayoutInflater เพื่อแปลง XML -> View
     }
 
     override fun getItemCount(): Int {
@@ -37,20 +42,64 @@ class MonthAdapter (
 
     override fun onBindViewHolder(holder: MonthViewHolder, position: Int) {
         val month = months[position]
-        holder.bind(month)
+        holder.bind(month, position)
     }
 
     inner class MonthViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tvMonth: TextView = itemView.findViewById(R.id.tvMonth)
         private val rvDays: RecyclerView = itemView.findViewById(R.id.rvDays)
 
-        fun bind(month: MonthData) {
+        fun bind(month: MonthData, position: Int) {
+            setupMonthName(month)
+            setupDaysRecyclerView(month, position)
+        }
+
+        private fun setupMonthName(month: MonthData){
             tvMonth.text = month.monthName
+        }
+
+        private fun setupDaysRecyclerView(month: MonthData, position: Int) {
             rvDays.layoutManager = GridLayoutManager(context, 7)
-            rvDays.adapter = DayAdapter(context, month.days, dayClickListener)
             rvDays.setHasFixedSize(true)
+
+            //Use the Map (dayAdapters) to store the DayAdapter of each month by the position.
+            //If it already exists, retrieve and use it.  // If not, create a new one and map it.
+            val adapter = dayAdapters.getOrPut(position) {
+                DayAdapter(
+                    context,
+                    month.days.toMutableList(),
+                    createDayClickListener(position)
+                )
+            }
+
+            rvDays.adapter = adapter
+        }
+
+        private fun createDayClickListener(monthIndex: Int): OnDayClickListener {
+            //When user press the Day in the Month -> Retrieve the onDayClick() -> Retrieve the handleDaySelection()
+            return object : OnDayClickListener {
+                override fun onDayClick(day: DayData) {
+                    handleDaySelection(monthIndex, day)
+                }
+            }
+        }
+
+        private fun handleDaySelection(currentMonth: Int, day: DayData) {
+            //Reset the Green marker //.let is a scope function that allows you to use the value of a variable within a { ... } block.
+            previousSelectedMonth?.let { previousMonthIndex ->
+                dayAdapters[previousMonthIndex]?.updateDayChange(null)
+            }
+
+            //Set the selected date
+            dayAdapters[currentMonth]?.updateDayChange(day.date)
+
+            //Save selected month and date data
+            previousSelectedMonth = currentMonth
+            previousSelectedDate = day.date
+
+            //Send event back
+            dayClickListener.onDayClick(day)
         }
     }
 }
-
 
